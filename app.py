f8eb1e12b34fd2370822fe7337b9636df12daa42
Ramón López <63748740+Ramonst18@users.noreply.github.com>
2022-09-18 15:42:43 -0700

    Base de datos V0.6

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template,request,session
 from flask_bootstrap import Bootstrap
-from basededatos import crear_bd, iniciar_bd, db
-from modelos import Eventos, Categorias
+from basededatos import iniciar_bd, db
+#from modelos import Eventos, Categorias
 from os.path import join, dirname
 from dotenv import load_dotenv
 from sqlalchemy.sql.expression import func
 from os import getenv
-
+import os
 app = Flask(__name__)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+cadenapsql = getenv('DATABASEURI')
+app.config['SQLALCHEMY_DATABASE_URI'] = cadenapsql
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = './static/images/'
+engine = iniciar_bd(app, cadenapsql)
 
 @app.route('/')
 def inicio():
